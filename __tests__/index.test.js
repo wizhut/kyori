84b7fa6bd773e8b08a566366b539c87c0fd0fda9
Vassilios Karakoidas <vassilios.karakoidas@wizhut.tech>
2024-09-22 16:47:01 +0300

    introducing tokenizer

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,9 +1,16 @@
 const t = require('tap');
 
-const kyoriDistance = require('../src/index.js');
+const kyori = require('../src/index.js');
 
 
-t.test('kyoriDistance tests', (t) => {  
-    t.equal(kyoriDistance('foo', 'foo'), 0);
+t.test('kyori.keyword_distance() tests', (t) => {  
+    t.equal(kyori.keyowrd_distance('foo', 'foo'), 0);
+    t.equal(kyori.keyowrd_distance('foo', 'bar'), 3);
+    t.end();
+});
+
+t.test('kyori.equals() tests', (t) => {
+    t.equal(kyori.equals('foo', 'foo'), true);
+    t.equal(kyori.equals('foo', 'bar'), false);
     t.end();
 });
