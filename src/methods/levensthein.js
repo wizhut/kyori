5803fc4d080a48578703a7908aaa55fb5239c39e
Vassilios Karakoidas <vassilios.karakoidas@wizhut.tech>
2024-09-30 00:31:07 +0300

    done with levensthein

--- src/methods/levensthein.js
+++ src/methods/levensthein.js
@@ -2,14 +2,23 @@
 wrapper for https://www.npmjs.com/package/fastest-levenshtein
  */
 const { distance, closest } = require('fastest-levenshtein')
+const { isNil } = require('../libs/lang.js');
 
 
 function fl_distance(terms, text) {
+    if (isNil(terms) || isNil(text)) {
+        return -1;
+    }
+
     return distance(terms, text);
 }
 
-function fl_closest() {
+function fl_closest(terms, arrayOfText) {
+    if (isNil(terms) || isNil(arrayOfText)) {
+        return null;
+    }
 
+    return closest(terms, arrayOfText);
 }
 
 
