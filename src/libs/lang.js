cacf59430e861b41084354689cc2c63ed95cbbc0
Vassilios Karakoidas <vassilios.karakoidas@wizhut.tech>
2024-09-29 23:13:15 +0300

    adding tests

---
+++ src/libs/lang.js
@@ -0,0 +1,10 @@
+
+
+function isNil(value) {
+    return value === null || value === undefined;
+}
+
+
+module.exports = {
+  isNil
+};
--- src/methods/levensthein.js
+++ src/methods/levensthein.js
@@ -4,8 +4,8 @@ wrapper for https://www.npmjs.com/package/fastest-levenshtein
 const { distance, closest } = require('fastest-levenshtein')
 
 
-function fl_distance() {
-
+function fl_distance(terms, text) {
+    return distance(terms, text);
 }
 
 function fl_closest() {
