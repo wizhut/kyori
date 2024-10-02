f27dcb9661a7155cfaf05a19aa4c144d42c27e9e
Vassilios Karakoidas <vassilios.karakoidas@wizhut.tech>
2024-10-02 10:57:33 +0300

    removed some methods that did not made sense; fixed tests

--- __tests__/kyori.test.js
+++ __tests__/kyori.test.js
@@ -4,7 +4,8 @@ const {kyori} = require('../src/methods/kyori.js');
 
 
 t.test('kyori/keyword_distance() tests', (t) => {
-    t.equal(kyori.keyword_distance('foo', 'foo'), 0);
+    console.log(kyori);
+    t.equal(kyori.distance.keyword('foo', 'foo'), 0);
     //t.equal(kyori.keyword_distance('foo', 'bar'), 3);
     t.end();
 });
