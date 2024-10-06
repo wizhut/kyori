417066039bdf3aee02a7977a20f64d3f073d5305
Vassilios Karakoidas <vassilios.karakoidas@wizhut.tech>
2024-10-06 16:33:58 +0300

    succesfully ported levensthein

--- src/methods/levensthein.js
+++ src/methods/levensthein.js
@@ -1,17 +1,30 @@
-/*
-wrapper for https://www.npmjs.com/package/fastest-levenshtein
- */
-const levensthein = require('talisman/metrics/levenshtein');
-
-const { isNil } = require('../libs/lang.js');
+const { allAreNotNil } = require('../libs/lang.js');
 
 
 function fl_distance(terms, text) {
-    if (isNil(terms) || isNil(text)) {
+    if (!allAreNotNil([terms, text])) {
         return -1;
     }
 
-    return levensthein(terms, text);
+    let t = [], m = terms.length, n = text.length;
+
+    for (let j = 0; j <= n; j++) {
+        t[j] = j;
+    }
+
+    let u;
+
+    for (let i = 1; i <= m; i++) {
+        let j = 1;
+
+        for (u = [i]; j <= n; j++) {
+            u[j] = terms[i - 1] === text[j - 1] ? t[j - 1] : Math.min(t[j - 1], t[j], u[j - 1]) + 1;
+        }
+
+        t = u;
+    }
+    
+    return u[n];
 }
 
 
