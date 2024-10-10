16711c7a74dcadebc39bf3e64bba8ee29f32a3b1
Vassilios Karakoidas <vassilios.karakoidas@wizhut.tech>
2024-10-10 12:10:03 +0300

    fixed broken test

--- __tests__/kyori.test.js
+++ __tests__/kyori.test.js
@@ -3,8 +3,8 @@ const t = require('tap');
 const { kyori } = require('../src/methods/similarity/kyori.js');
 
 
-t.test('kyori/keyword_distance() tests', (t) => {
-    t.equal(kyori.distance.keyword('foo', 'foo'), 0);
+t.test('kyori/similarity() tests', (t) => {
+    t.equal(kyori.similarity('foo', 'foo'), 0);
     //t.equal(kyori.keyword_distance('foo', 'bar'), 3);
     t.end();
 });
